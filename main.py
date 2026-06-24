e859e095fc02ff4520481ecf9de7d45395f39aab
Keyla Islas <keylaislas@MacBook-Air-de-Keyla.local>
2026-06-24 17:02:29 -0600

    Validacion correo y dominio

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from dotenv import load_dotenv
 
 import pandas as pd
 import re
+import numpy as np
 from google.cloud import bigquery
+from email_validator import validate_email, EmailNotValidError
 
 ## Extrae variables del .env
 load_dotenv()
@@ -23,63 +25,101 @@ PASSWORD = os.environ.get("PASSWORD", "")
 ##  En GCP se usa client = bigquery.Client(project=PROJECT_ID)
 client = bigquery.Client(project=os.environ["GCP_PROJECT"])
 
-query = '''SELECT DISTINCT Proveedor, Nombre,
-COALESCE(
-  NULLIF(Email1, ''),
-  NULLIF(Email2, ''),
-  NULLIF(Email3, '')
-) AS Email FROM `finsadashboard.raw_data.Proveedores`
-WHERE COALESCE(
-  NULLIF(Email1, ''),
-  NULLIF(Email2, ''),
-  NULLIF(Email3, '')
-)  is not NULL'''
+# # query = '''SELECT DISTINCT Proveedor, Nombre,
+# # COALESCE(
+# #   NULLIF(Email1, ''),
+# #   NULLIF(Email2, ''),
+# #   NULLIF(Email3, '')
+# # ) AS Email FROM `finsadashboard.raw_data.Proveedores`
+# # WHERE 
+# # COALESCE(
+# #   NULLIF(Email1, ''),
+# #   NULLIF(Email2, ''),
+# #   NULLIF(Email3, '')
+# # ) LIKE '%@%' 
 
-## Query de prueba para envío a destinatario fijo
-# query = '''SELECT DISTINCT Proveedor, Nombre,
-# 'daniel.perez@danuanalitica.com' AS Email 
-# FROM `finsadashboard.raw_data.Proveedores` LIMIT 3
-# '''
+# # --LIMIT 100
+# # '''
+
+# Query de prueba para envío a destinatario fijo
+query = '''SELECT DISTINCT Proveedor, Nombre,
+'leonardo.laureles@danuanalitica.com' AS Email 
+--CASE WHEN Proveedor = 10096 THEN 'fmartinez@finsa.com.mx' ELSE 'ruben.garza@finsa.com.mx' END AS Email
+FROM `finsadashboard.raw_data.Proveedores` 
+--LIMIT 3
+WHERE Proveedor IN (10096,10099)
+'''
 
 correos = client.query(query).to_dataframe()
-correos
 
-#FALTA VALIDAR CORREOS CON REGEX
 
-# ─── BIGQUERY ─────────────────────────────────────────────────────────────────
-def get_backorder_mty(provider_name: str ) -> pd.DataFrame:
-    """
-    Retrieves backorder records from BigQuery for a specific provider.
-    """
-    #client = bigquery.Client(project=PROJECT_ID)
-    
-    query = """
+query = """
         SELECT 
-            NOMBRE_PROVEEDOR,
+            EDP,
+            FOLIO AS ORDEN_COMPRA,
+            PARTIDA,
             FECHA_ALTA,
-            SUCURSAL,
-            ALMACEN,
             ARTICULO,
+            DESCRIPCION,
             CANTIDAD,
             CANTIDAD_RECIBIDA,
             BACKORDER,
             UNIDAD,
             COSTO,
             FECHA_EMBARQUE,
-            DIAS_RETRASO_EMBARQUE
+            DIAS_RETRASO_EMBARQUE,
+            NOMBRE_SUCURSAL,
+            NOMBRE_COMPRADOR,
+            NOMBRE_PROVEEDOR,
+            PROVEEDOR,
+            COMPRADOR,
+            SUCURSAL
         FROM `finsadashboard.mrts.mrts_backorder_MTY`
         WHERE BACKORDER > 0 
-          AND NOMBRE_PROVEEDOR = @provider_name
-        ORDER BY NOMBRE_PROVEEDOR, FECHA_ALTA DESC
+          AND DIAS_RETRASO_EMBARQUE > 0
+          AND  PROVEEDOR IN (10096,10099)
+        ORDER BY NOMBRE_PROVEEDOR, FECHA_ALTA
     """
+
+backorder = client.query(query).to_dataframe()
+backorder
+
+loop_values = backorder[['NOMBRE_COMPRADOR','NOMBRE_PROVEEDOR', 'NOMBRE_SUCURSAL', 'NOMBRE_COMPRADOR', 'PROVEEDOR', 'COMPRADOR', 'SUCURSAL']].drop_duplicates()
+loop_values = loop_values.merge(correos, left_on='PROVEEDOR', right_on='Proveedor', how='left')
+# ─── DEF CORREOS_CLEAN ─────────────────────────────────────────────────────────────────
+
+def clean_email_addresses(correos: pd.DataFrame) -> pd.DataFrame:
+    extraido = correos['Email'].str.extract(r'\[(.*?)\]', expand=False)
+    correos['Clean'] = extraido.fillna(correos['Email'])
+    extraido = correos['Clean'].str.extract(r'<(.*?)>', expand=False)
+    correos['Clean'] = (
+    extraido
+    .fillna(correos['Clean'])
+    .str.replace(r'[\[\]<>]', '', regex=True)  # eliminar caracteres especiales
+    .str.rsplit(' ', n=1).str[-1]              # quedarse con la última palabra
+    .str.rsplit(':', n=1).str[-1]              # eliminar prefijo tipo "mailto:"
+)
+    patron = r'([a-zA-Z0-9Ññ._-]+@[a-zA-Z0-9Ññ_-]+(?:\.[a-zA-Z]{2,})+)'    
+    correos['Clean'] = correos['Clean'].str.extract(patron)
+    correos = correos.dropna(subset=['Clean'])
     
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("provider_name", "STRING", provider_name)
-        ]
-    )
-    
-    df = client.query(query, job_config=job_config).to_dataframe()
+    return correos
+
+# ─── DEF VALIDAR DOMINIO ─────────────────────────────────────────────────────────────────
+def verificar_existencia_correo(correo):
+    try:
+        informacion = validate_email(correo, check_deliverability=True)
+        return True
+    except EmailNotValidError as e:
+        return False
+
+
+# ─── DEF DATA ─────────────────────────────────────────────────────────────────
+def get_backorder(provider_name: str, branch_name: str, buyer_name: str, df: pd.DataFrame) -> pd.DataFrame:
+
+    df = df[(df['NOMBRE_PROVEEDOR'] == provider_name) &
+            (df['NOMBRE_SUCURSAL'] == branch_name) &
+            (df['NOMBRE_COMPRADOR'] == buyer_name)]
     return df
 
 # ─── CORREO ───────────────────────────────────────────────────────────────────
@@ -163,10 +203,14 @@ def send_email_backorder(df: pd.DataFrame, Proveedor: str):
 
 if __name__ == "__main__":
     try:
-      for row in correos.itertuples():
+      loop_values = clean_email_addresses(loop_values)
+      resultados = loop_values['Email'].apply(verificar_existencia_correo)
+      loop_values['es_valido'] = resultados
+
+      for row in loop_values.itertuples():
         print(row.Nombre, row.Email)
         print("Obteniendo datos de backorder",row.Nombre)
-        df = get_backorder_mty(row.Nombre)
+        df = get_backorder(row.NOMBRE_PROVEEDOR, row.NOMBRE_SUCURSAL, row.NOMBRE_COMPRADOR, backorder)
         print(df)
         
         print("\nEnviando reporte por correo...")
