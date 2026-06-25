581ccd3cbdd2c75d515eceeaeba7ba1424692b27
Keyla Islas <keylaislas@MacBook-Air-de-Keyla.local>
2026-06-25 10:37:40 -0600

    Segmentación por proveedor, sucursal, comprador, cambio en .env para multiples emisores, valores manuales de emisor en tablas (para considerar mi correo, aun no hay configuracion de los correos de los compradores

--- main.py
+++ main.py
@@ -13,14 +13,18 @@ from dotenv import load_dotenv
 import pandas as pd
 import re
 import numpy as np
+import ast
 from google.cloud import bigquery
 from email_validator import validate_email, EmailNotValidError
 
 ## Extrae variables del .env
 load_dotenv()
 PROJECT_ID = os.environ["GCP_PROJECT"]
-EMISOR = os.environ.get("EMISOR", "")
-PASSWORD = os.environ.get("PASSWORD", "")
+# EMISOR = os.environ.get("EMISOR", "")
+# PASSWORD = os.environ.get("PASSWORD", "")
+BUYERS_PASSWORD = os.environ.get("BUYERS_PASSWORD", "")
+BUYERS_PASSWORD = ast.literal_eval(BUYERS_PASSWORD)
+buyer_passwords = pd.DataFrame(list(BUYERS_PASSWORD.items()), columns=['Usuario_ID', 'Password'])
 
 ##  En GCP se usa client = bigquery.Client(project=PROJECT_ID)
 client = bigquery.Client(project=os.environ["GCP_PROJECT"])
@@ -52,7 +56,6 @@ WHERE Proveedor IN (10096,10099)
 
 correos = client.query(query).to_dataframe()
 
-
 query = """
         SELECT 
             EDP,
@@ -72,8 +75,9 @@ query = """
             NOMBRE_COMPRADOR,
             NOMBRE_PROVEEDOR,
             PROVEEDOR,
-            COMPRADOR,
-            SUCURSAL
+            1 AS COMPRADOR,
+            "keyla.islas@danuanalitica.com" AS Email_COMPRADOR,
+            SUCURSAL, PEDIDO
         FROM `finsadashboard.mrts.mrts_backorder_MTY`
         WHERE BACKORDER > 0 
           AND DIAS_RETRASO_EMBARQUE > 0
@@ -82,10 +86,14 @@ query = """
     """
 
 backorder = client.query(query).to_dataframe()
-backorder
 
-loop_values = backorder[['NOMBRE_COMPRADOR','NOMBRE_PROVEEDOR', 'NOMBRE_SUCURSAL', 'NOMBRE_COMPRADOR', 'PROVEEDOR', 'COMPRADOR', 'SUCURSAL']].drop_duplicates()
+
+loop_values = backorder[['NOMBRE_COMPRADOR','NOMBRE_PROVEEDOR', 'NOMBRE_SUCURSAL', 'NOMBRE_COMPRADOR', 'PROVEEDOR', 'COMPRADOR', 'SUCURSAL', 'Email_COMPRADOR']].drop_duplicates()
 loop_values = loop_values.merge(correos, left_on='PROVEEDOR', right_on='Proveedor', how='left')
+loop_values = loop_values.merge(buyer_passwords, left_on='COMPRADOR', right_on='Usuario_ID', how='left')
+loop_values = loop_values[['NOMBRE_PROVEEDOR','NOMBRE_COMPRADOR', 'NOMBRE_SUCURSAL', 'Email_COMPRADOR', 'Email', 'Password']]
+
+
 # ─── DEF CORREOS_CLEAN ─────────────────────────────────────────────────────────────────
 
 def clean_email_addresses(correos: pd.DataFrame) -> pd.DataFrame:
@@ -108,7 +116,7 @@ def clean_email_addresses(correos: pd.DataFrame) -> pd.DataFrame:
 # ─── DEF VALIDAR DOMINIO ─────────────────────────────────────────────────────────────────
 def verificar_existencia_correo(correo):
     try:
-        informacion = validate_email(correo, check_deliverability=True)
+        validate_email(correo, check_deliverability=True)
         return True
     except EmailNotValidError as e:
         return False
@@ -120,10 +128,12 @@ def get_backorder(provider_name: str, branch_name: str, buyer_name: str, df: pd.
     df = df[(df['NOMBRE_PROVEEDOR'] == provider_name) &
             (df['NOMBRE_SUCURSAL'] == branch_name) &
             (df['NOMBRE_COMPRADOR'] == buyer_name)]
+    df = df[['FECHA_ALTA','ORDEN_COMPRA','PEDIDO','ARTICULO','DESCRIPCION','EDP','PARTIDA','CANTIDAD','CANTIDAD_RECIBIDA','BACKORDER','UNIDAD','FECHA_EMBARQUE','DIAS_RETRASO_EMBARQUE']]
+
     return df
 
 # ─── CORREO ───────────────────────────────────────────────────────────────────
-def send_email_backorder(df: pd.DataFrame, Proveedor: str):
+def send_email_backorder(df: pd.DataFrame, Email_proveedor: str, Email_comprador: str, Comprador: str,  Sucursal: str, Password: str) -> None:
     """
     Sends the backorder DataFrame as a styled HTML table via email.
     """
@@ -172,28 +182,30 @@ def send_email_backorder(df: pd.DataFrame, Proveedor: str):
     </head>
     <body>
     <p>Buen día,</p>
-    <p>Te comparto el reporte de backorder de MTY para su revisión.</p>
-    <h2>Reporte de Backorder - MTY</h2>
+    <p>Te comparto el reporte de backorder de la sucursal {Sucursal} para su revisión.</p>
+    <h2>Reporte de Backorder - {Sucursal}</h2>
     {df.to_html(index=False)}
     <br>
+    <p>Favor de revisar el reporte y tomar las acciones necesarias.</p>
+    <p>Comprador encargado: {Comprador}</p>
     <p style="font-size: 11px; color: #777777;">Este es un correo automático generado por el sistema.</p>
     </body>
     </html>
     """
 
     msg = MIMEMultipart("alternative")
-    msg["Subject"] = "Reporte de Backorder MTY"
-    msg["From"]    = EMISOR
-    msg["To"]      = Proveedor #", ".join([r.strip() for r in RECEPTOR.split(",")])
+    msg["Subject"] = f"Reporte de Backorder - {Sucursal}"
+    msg["From"]    = Email_comprador
+    msg["To"]      = Email_proveedor #", ".join([r.strip() for r in RECEPTOR.split(",")])
     msg.attach(MIMEText(html, "html"))
 
-    if not EMISOR or not PASSWORD:
-        print("⚠️ Advertencia: EMISOR o PASSWORD no están configurados en el archivo .env. No se puede enviar el correo.")
+    if not Email_comprador or not Password:
+        print("⚠️ Advertencia: Email_comprador o Password no están configurados en el archivo .env. No se puede enviar el correo.")
         return
 
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(EMISOR, PASSWORD)
+            smtp.login(Email_comprador, Password)
             smtp.send_message(msg)
         print("✅ Correo enviado")
     except Exception as e:
@@ -206,14 +218,18 @@ if __name__ == "__main__":
       loop_values = clean_email_addresses(loop_values)
       resultados = loop_values['Email'].apply(verificar_existencia_correo)
       loop_values['es_valido'] = resultados
+      loop_values = loop_values[loop_values['es_valido'] == True]
+      print(loop_values)
 
       for row in loop_values.itertuples():
-        print(row.Nombre, row.Email)
-        print("Obteniendo datos de backorder",row.Nombre)
+        print(row.NOMBRE_PROVEEDOR, row.Email)
+        print("Obteniendo datos de backorder",row.NOMBRE_PROVEEDOR)
         df = get_backorder(row.NOMBRE_PROVEEDOR, row.NOMBRE_SUCURSAL, row.NOMBRE_COMPRADOR, backorder)
         print(df)
         
         print("\nEnviando reporte por correo...")
-        send_email_backorder(df, row.Email)
+        send_email_backorder(df, row.Email, row.Email_COMPRADOR, row.NOMBRE_COMPRADOR, row.NOMBRE_SUCURSAL, row.Password)
+        
+        (df, row.Email)
     except Exception as e:
         print("Ocurrió un error al consultar BigQuery:", e)
